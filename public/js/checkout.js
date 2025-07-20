d709c89a7c67dcd7d91c10ac958a92c39c43fbde
Caleb Pirkle <calebpirkle2@gmail.com>
2025-07-19 21:27:29 -0500

    removed code

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -15,27 +15,6 @@ serviceForm.addEventListener('submit', onFormSubmit);
 
 // async function to handle for submission
 async function onFormSubmit(event) {
-    event.preventDefault();
-
-    // data from form
-    const data = {
-        name: nameInput.value,
-        email: emailInput.value,
-        phone: phoneInput.value,
-        eventDate: eventDateInput.value,
-        service: serviceSelect.value
-    }
-
-    // make a POST request to server end-point
-    const res = await fetch('/create-checkout-session', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    });
-
-    // get url from server response and redirect user to stripe checkout
-    const { url } = await res.json();
-    window.location.href = url;
 }
 
 
